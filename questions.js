0575b5bd0381ba8c912df66b7e3bec251eae20f0
matt sheets <matthewsheets05@gmail.com>
2022-03-06 20:19:40 -0500

    deploy wip

---
+++ questions.js
@@ -0,0 +1,59 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: " Why so JavaScript and Java have similar name?",
+    answer: "JavaScript's syntax is loosely based on Java's",
+    options: [
+      "JavaScript is a stripped-down version of Java",
+      "JavaScript's syntax is loosely based on Java's",
+      "They both originated on the island of Java",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "__________ JavaScript is also called SERVER-side JavaScript.",
+    answer: "LiveWire",
+    options: [
+      "LiveWire",
+      "Navigator",
+      "Native",
+      "Non-Native"
+    ]
+  },
+    {
+    numb: 3,
+    question: " __________ JavaScript is also called CLIENT-side JavaScript",
+    answer: "Navigator",
+    options: [
+      "Navigator",
+      "LiveWire",
+      "Native",
+      "Non-Native"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does API stand for?",
+    answer: "Application programming interface",
+    options: [
+      "Active Pharmaceutical Ingredient",
+      "Academic Performance Index",
+      "American Petroleum Institute",
+      "Application programming interface"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following can't be done with client-side JavaScript?",
+    answer: "Storing the form's contents to a database file on the server",
+    options: [
+      "Validating a form",
+      "Sending a form's contents by email",
+      "Storing the form's contents to a database file on the server",
+      "None of the above"
+    ]
+  },
+
+];
